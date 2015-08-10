ccd8fe17b56de4dafda2ec5e7dcbf06aabc1146b
QianChengLong <qian_cheng_long@163.com>
2015-08-10 16:41:04 +0800

    modify

--- upload.cmd
+++ upload.cmd
@@ -3,16 +3,17 @@
 SETLOCAL
 
 SET SSH=E:\Soft\rsync\bin\cwRsync_5.4.1_x86_Free\ssh.exe
-SET SSH_KEY=/cygdrive/E/pan/Soft/rsync/bin/cwRsync_5.4.1_x86_Free/id_rsa
+SET SSH_KEY=/cygdrive/E/Soft/rsync/bin/cwRsync_5.4.1_x86_Free/id_rsa
 SET SSH_OPTION=-i %SSH_KEY% -o StrictHostKeyChecking=no
 
 SET RSYNC=E:\Soft\rsync\bin\cwRsync_5.4.1_x86_Free\rsync.exe
-SET RSYNC_OP=-avzP -e "%SSH% %SSH_OPTION%" --delete --exclude={build,.git,doc,test,.swp}
+SET RSYNC_OP=-rvzP -e "%SSH% %SSH_OPTION%" --delete --exclude={build,.git,doc,test,.swp}
 
-SET RHOST=120.25.250.237
-SET RUSER=wallace
-SET RPATH=%RUSER%@%RHOST%:/home/wallace/
+rem SET RHOST=app.gupiaoxianji.com
+SET RHOST=debug.gupiaoxianji.com
+SET RUSER=root
+SET RPATH=%RUSER%@%RHOST%:/opt/lilylog
 
-SET LPATH=/cygdrive/E/Programming/SourceCode/LilyLog
+SET LPATH=/cygdrive/E/Programming/SourceCode/LilyLog/
 
 %RSYNC% %RSYNC_OP% %LPATH% %RPATH%
